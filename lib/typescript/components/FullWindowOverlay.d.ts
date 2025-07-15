9af78f107bb2a8bb73cc7f17297f8d8849b50325
Dima <dmitrijs.sokolovs@thomann.io>
2025-07-15 13:07:38 +0200

    add lib file

---
+++ lib/typescript/components/FullWindowOverlay.d.ts
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react';
+type FullWindowOverlayProps = {
+    children: ReactNode;
+    unstable_accessibilityContainerViewIsModal?: boolean;
+    accessibilityEnabled?: boolean;
+};
+declare function FullWindowOverlay(props: FullWindowOverlayProps): React.JSX.Element;
+export default FullWindowOverlay;
+//# sourceMappingURL=FullWindowOverlay.d.ts.map
---
+++ lib/typescript/fabric/FullWindowOverlayNativeComponent.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="react-native/types/modules/Codegen" />
+import type { ViewProps } from 'react-native';
+import { WithDefault } from 'react-native/Libraries/Types/CodegenTypes';
+export interface NativeProps extends ViewProps {
+    accessibilityContainerViewIsModal?: WithDefault<boolean, true>;
+    accessibilityEnabled?: WithDefault<boolean, false>;
+}
+declare const _default: import("react-native/Libraries/Utilities/codegenNativeComponent").NativeComponentType<NativeProps>;
+export default _default;
+//# sourceMappingURL=FullWindowOverlayNativeComponent.d.ts.map
